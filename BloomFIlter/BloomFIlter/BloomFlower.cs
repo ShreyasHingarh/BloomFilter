81536359c12dbf4a15bc74402f85e9e807d5fbaf
shreyas.hingarh2@gmail.com <shreyas.hingarh2@gmail.com>
2022-12-26 18:28:00 -0800

    Finished

--- BloomFIlter/BloomFIlter/BloomFlower.cs
+++ BloomFIlter/BloomFIlter/BloomFlower.cs
@@ -6,7 +6,76 @@ using System.Threading.Tasks;
 
 namespace BloomFIlter
 {
-    internal class BloomFlower
+    internal class BloomFlower<T>
     {
+       
+            public int Cap;
+            public byte[] Filter;
+            List<Func<T,int>> Funcs;
+            public BloomFlower(int cap,int StartNumOfFuncs)
+            {
+                Cap = cap;
+                Filter = new byte[cap];
+                Funcs = new List<Func<T, int>>();
+
+                if (Math.Abs(StartNumOfFuncs) >= 3)
+                {
+                    Funcs.Add(HashFuncThree);
+                    StartNumOfFuncs = 2;
+                } 
+                if(Math.Abs(StartNumOfFuncs) == 2)
+                {
+                    Funcs.Add(HashFuncTwo);
+                    StartNumOfFuncs = 1;
+                }
+                if (Math.Abs(StartNumOfFuncs) == 1)
+                {
+                    Funcs.Add(HashFuncOne);
+                }
+
+            }
+
+            public void LoadHashFunc(Func<T, int> hashFunc)
+            {
+                Funcs.Add(hashFunc);
+            }
+
+            public void Insert(T item)
+            {
+                foreach(var thing in Funcs)
+                {
+                    Filter[thing(item) % Cap] = 1;
+                }
+            }
+
+            public bool ProbablyContains(T item)
+            {
+                foreach(var thing in Funcs)
+                {
+                    int index = thing(item) % Cap;
+                    if (Filter[index] != 1)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            private int HashFuncOne(T item)
+            {
+                return item.GetHashCode() % Cap;
+            }
+
+            private int HashFuncTwo(T item)
+            {
+                return ((item.GetHashCode() + 3) / 5) % Cap;
+            }
+
+            private int HashFuncThree(T item)
+            {
+                return ((item.GetHashCode() + 5) * 3) % Cap;
+            }
+        
+
     }
 }
--- BloomFIlter/BloomFIlter/Program.cs
+++ BloomFIlter/BloomFIlter/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int length = 20;
+            List<int> nums = new List<int>();
+            BloomFlower<int> thing = new BloomFlower<int>(length * 2, 3);
+            for (int i = 0;i < length;i++)
+            {
+                nums.Add(i);
+                thing.Insert(i);
+            }
+            foreach(var item in thing.Filter)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(thing.ProbablyContains(67));
         }
     }
 }
